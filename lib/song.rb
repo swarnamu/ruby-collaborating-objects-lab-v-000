90ae691cf9b2f18b15060b90273dcce1888119f9
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-05-15 13:48:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,4 +11,24 @@ class Song
       new_song.artist_name = artist
       new_song.save
     end
+
+    def self.find_by_artist(artist)
+    Song.all.select do | song |
+      song.artist == artist
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+  end
+
+  def save
+    @@all << self
+    self
+  end
+
 end
