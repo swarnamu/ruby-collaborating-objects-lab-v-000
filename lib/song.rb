3cb3f97e216ee49c4b828a6be8a1306f3063a000
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-05-15 14:00:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,30 +5,16 @@ class Song
       @name = name
     end
 
-    def save
-		@@all << self
-	end
+    def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
 
-	def songs
-		@songs
-	end
-
-	def add_song(song)
-		@songs << song
-	end
-
-	def self.find_or_create_by_name(name)
-		@@all.each  do |artist|
-			if artist.name == name
-				return artist
-			else
-				return self.new(name)
-			end
-		end
-	end
-
-	def self.all
-		@@all
-	end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 
 end
